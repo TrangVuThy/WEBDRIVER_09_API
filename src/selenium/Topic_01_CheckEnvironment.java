c661a0fdebe6f962b990af10408644f0fbdc6e2b
TrangVuThy <vutrangptit@gmail.com>
2019-02-26 16:15:38 +0700

    Push code to Github

---
+++ src/selenium/Topic_01_CheckEnvironment.java
@@ -0,0 +1,38 @@
+package selenium;
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeTest;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class Topic_01_CheckEnvironment {
+	WebDriver driver;
+	
+	 @BeforeTest
+	  public void beforeTest() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://live.guru99.com/");
+	  }
+
+  @Test
+  public void TC_01_CheckUrl() {
+	  String homePageUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(homePageUrl, "http://live.guru99.com/");
+  }
+  @Test
+  public void TC_01_CheckTitle() {
+	  String homePageTitle = driver.getTitle();
+	  Assert.assertEquals(homePageTitle, "Home page");
+  }
+ 
+  @AfterTest
+  public void afterTest() {
+	  driver.quit(); 
+  }
+  
+}
